b9bf27b13af5cc3a317fd6e77752652ae5a53b13
Evan Jones <evan_jones@mac.com>
2023-07-21 15:48:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# `term_2048`: 2048 in the terminal
+
+This is a small implementation of Gabriele Cirulli's [2048 game](https://play2048.co/) in the terminal using Python. I wrote it to learn about using the [`curses`](https://docs.python.org/3/howto/curses.html)library in Python. I learned enough about `curses` to prefer the much smoother [`blessed`](https://blessed.readthedocs.io/en/latest/intro.html) library instead, so that's what's used here.
+
+## Installation
+
+```bash
+pip install term_2048
+```
+
+## Running `term_2048`
+
+Once you've installed the package, you can run the game from the terminal:
+
+```bash
+term_2048
+```
+
+Use `term_2048 --help` to print help options:
+
+```bash
+usage: term_2048 [-h] [-w WIDTH] [--height HEIGHT] [-s {S,M,L,XL}] [--start_value START_VALUE]
+
+2048 game in the terminal. Inspired by https://play2048.co/
+
+options:
+  -h, --help            show this help message and exit
+  -w WIDTH, --width WIDTH
+                        Width of the board (default: 4)
+  --height HEIGHT       Height of the board (default: 4)
+  -s {S,M,L,XL}, --size {S,M,L,XL}
+                        Board size (options: S, M, L, XL; default: M)
+  --start_value START_VALUE
+                        Highest starting tile value (default: 4)
+```
+
+## Compatibility
+
+I've only run this on MacOS so far, and the terminal management is pretty naive.
+Let me know if you have issues on a given OS or terminal setup.
+
+### Contact & bug reports
+
+Bug reports and pull requests are welcome!
+Feel free to contact me on [Github](https://github.com/etjones/term2048) or by email at [evan_t_jones@mac.com](mailto://evan_jones@mac.com).)
---
+++ tests/test_term_2048.py
@@ -0,0 +1,100 @@
+import unittest
+from term_2048.term_2048 import move, merge_tiles, GameAction
+
+class TestMoveFunction(unittest.TestCase):
+
+    def test_move_down(self):
+        board = [
+            [2, 2, 0, 4],
+            [0, 0, 0, 4],
+            [4, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        expected_board = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [2, 0, 0, 0],
+            [4, 2, 0, 8]
+        ]
+        expected_score = 8
+
+        actual_board, actual_score = move(board, GameAction.DOWN)
+        self.assertEqual(actual_board, expected_board)
+        self.assertEqual(actual_score, expected_score)
+
+    def test_move_up(self):
+        board = [
+            [2, 2, 0, 4],
+            [0, 0, 0, 4],
+            [4, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        expected_board = [
+            [2, 2, 0, 8],
+            [4, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        expected_score = 8
+        actual_board, actual_score = move(board, GameAction.UP)
+        self.assertEqual(actual_board, expected_board)
+        self.assertEqual(actual_score, expected_score)
+
+    def test_move_left(self):
+        board = [
+            [2, 2, 0, 4],
+            [0, 0, 0, 4],
+            [4, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        expected_board = [
+            [4, 4, 0, 0],
+            [4, 0, 0, 0],
+            [4, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        expected_score = 4
+        actual_board, actual_score = move(board, GameAction.LEFT)
+        self.assertEqual(actual_board, expected_board)
+        self.assertEqual(actual_score, expected_score)
+
+    def test_move_right(self):
+        board = [
+            [2, 2, 0, 4],
+            [0, 0, 0, 4],
+            [4, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        expected_board = [
+            [0, 0, 4, 4],
+            [0, 0, 0, 4],
+            [0, 0, 0, 4],
+            [0, 0, 0, 0]
+        ]
+
+        expected_score = 4
+        actual_board, actual_score = move(board, GameAction.RIGHT)
+        self.assertEqual(actual_board, expected_board)
+        self.assertEqual(actual_score, expected_score)
+
+class TestMergeFunction(unittest.TestCase):
+    def test_merge(self):
+        rows = [
+            [2, 2, 0, 4],
+            [2, 0, 0, 2],
+            [4, 4, 2, 2], 
+        ]
+        expected_rows = [
+            ([4, 4, 0, 0], 4),
+            ([4, 0, 0, 0], 4),
+            ([8, 4, 0, 0], 12),
+        ]
+        for row, (expected_row, expected_score) in zip(rows, expected_rows):
+            actual_row, score = merge_tiles(row)
+
+            self.assertEqual(actual_row, expected_row)
+            self.assertEqual(score, expected_score)
+
+
+if __name__ == "__main__":
+    unittest.main()
